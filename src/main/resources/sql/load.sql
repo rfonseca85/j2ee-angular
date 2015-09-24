ec7d452aef866dee4ddf795a03196f930040ba0e
Rafael Fonseca <rfonseca85@yahoo.ca>
2015-09-23 22:19:54 -0400

    First angular page for books

---
+++ src/main/resources/sql/load.sql
@@ -0,0 +1,22 @@
+INSERT INTO BOOK("ID", "NAME", "DESCRIPTION", "IMAGEURL") VALUES (1, 'Dom Quixote', 'Miguel de Cervantes', 'http://www.revistabula.com/wp/wp-content/uploads/2015/08/Dom-Quixote-de-La-Mancha.png')
+INSERT INTO BOOK("ID", "NAME", "DESCRIPTION", "IMAGEURL") VALUES (2, 'Guerra e Paz', 'Liev Tolstói'      , 'http://www.revistabula.com/wp/wp-content/uploads/2015/08/Guerra-e-Paz.jpg')
+INSERT INTO BOOK("ID", "NAME", "DESCRIPTION", "IMAGEURL") VALUES (3, 'A Montanha Mágica', 'Thomas Mann'  , 'http://www.revistabula.com/wp/wp-content/uploads/2015/08/A-Montanha-Magica.png')
+INSERT INTO BOOK("ID", "NAME", "DESCRIPTION", "IMAGEURL") VALUES (4, 'Ulisses', 'James Joyce'			, 'http://www.revistabula.com/wp/wp-content/uploads/2015/08/Ulisses.png')
+INSERT INTO BOOK("ID", "NAME", "DESCRIPTION", "IMAGEURL") VALUES (5, 'Cem Anos de Solidão', 'Gabriel García Márquez', 'http://www.revistabula.com/wp/wp-content/uploads/2015/08/Cem-Anos-de-Solidao.png')
+INSERT INTO BOOK("ID", "NAME", "DESCRIPTION", "IMAGEURL") VALUES (6, 'A Divina Comédia', 'Dante Alighieri', 'http://www.revistabula.com/wp/wp-content/uploads/2015/08/A-Divina-Comedia.png')
+INSERT INTO BOOK("ID", "NAME", "DESCRIPTION", "IMAGEURL") VALUES (7, 'Em Busca do Tempo Perdido', 'Marcel Proust', 'http://www.revistabula.com/wp/wp-content/uploads/2015/08/Em-Busca-do-Tempo-Perdido.png')
+INSERT INTO BOOK("ID", "NAME", "DESCRIPTION", "IMAGEURL") VALUES (8, 'O Som e a Fúria', 'William Faulkner', 'http://www.revistabula.com/wp/wp-content/uploads/2015/08/O-Som-e-a-Furia.png')
+INSERT INTO BOOK("ID", "NAME", "DESCRIPTION", "IMAGEURL") VALUES (9, 'O Homem sem Qualidades', 'Robert Musil', 'http://www.revistabula.com/wp/wp-content/uploads/2015/08/O-Homem-sem-Qualidades.png')
+INSERT INTO BOOK("ID", "NAME", "DESCRIPTION", "IMAGEURL") VALUES (10,'O Processo', 'Franz Kafka', 'http://www.revistabula.com/wp/wp-content/uploads/2015/08/O-Processo.png')
+
+
+
+
+
+
+
+
+
+
+
+
